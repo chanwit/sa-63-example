1eb0d3a21c016cac7aa1656ba9116046247278ed
Chanwit Kaewkasi <chanwit@gmail.com>
2020-07-30 13:34:34 +0700

    Add files via upload

---
+++ backend/go.mod
@@ -0,0 +1,20 @@
+module github.com/chanwit/app
+
+go 1.14
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/facebookincubator/ent v0.2.7
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-openapi/spec v0.19.9 // indirect
+	github.com/go-openapi/swag v0.19.9 // indirect
+	github.com/mailru/easyjson v0.7.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0
+	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
+	github.com/swaggo/gin-swagger v1.2.0
+	github.com/swaggo/swag v1.6.7
+	github.com/urfave/cli/v2 v2.2.0 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/tools v0.0.0-20200724022722-7017fd6b1305 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/chanwit/app/controllers"
+	_ "github.com/chanwit/app/docs"
+	"github.com/chanwit/app/ent"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @title SUT SA Example API
+// @version 1.0
+// @description This is a sample server for SUT SE 2563
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationUrl https://example.com/oauth/authorize
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password OAuth2Password
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl https://example.com/oauth/token
+// @authorizationUrl https://example.com/oauth/authorize
+// @scope.admin Grants read and write access to administrative information
+func main() {
+	router := gin.Default()
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("fail to open sqlite3: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	v1 := router.Group("/api/v1")
+	controllers.NewUserController(v1, client)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.Run()
+}
